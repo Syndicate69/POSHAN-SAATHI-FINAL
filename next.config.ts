47af77cf274cf6b23aae032a46cbc49d5a77b3d3
Rajon Shrestha <107099112+Syndicate69@users.noreply.github.com>
2026-03-25 10:44:30 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';
-const withPWA = require ('next-pwa').default({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
     register: true,
   skipWaiting: true,
