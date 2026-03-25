cb2500d8c0c16234830efe83a32c5c25bb27fbf5
Rajon Shrestha <107099112+Syndicate69@users.noreply.github.com>
2026-03-25 10:37:23 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
-const withPWA = require ('next-pwa')({
+const withPWA = require ('@ducanh2912/next-pwa').default({
   dest: 'public',
-    register: 'true',
-  skipWaiting: 'true',
+    register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
   
